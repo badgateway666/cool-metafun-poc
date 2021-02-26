9c0113023d2dc9bdaf2fbac2abf2c3c17278bc2f
badgateway666 <badgateway@das-labor.org>
2021-02-27 00:34:34 +0100

    Refactored to return instances of all objects

--- metafun_test.py
+++ metafun_test.py
@@ -5,12 +5,15 @@ import importlib
 import metafun
 
 
-def magic(module):
-    for mod in pkgutil.walk_packages(module.__path__, f"{module.__name__}."):
-        if not mod.ispkg:   # All packages are modules, but not all modules are packages
-            imported_mod = importlib.import_module(mod.name)
-            [cls()() for name, cls in inspect.getmembers(imported_mod, inspect.isclass)]
+def magic(root_module):
+    objs = []
+    for mod in pkgutil.walk_packages(root_module.__path__, f"{root_module.__name__}."):
+        if not mod.ispkg:  # All packages are modules, but not all modules are packages
+            objs.extend([cls() for name, cls in inspect.getmembers(importlib.import_module(mod.name), inspect.isclass)])
+    return objs
 
 
 if __name__ == '__main__':
-    magic(metafun)
+    all_the_objects = magic(metafun)
+    for o in all_the_objects:
+        o()
