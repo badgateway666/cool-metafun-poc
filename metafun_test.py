3c3daa99c035aa6442a911c34090c150c8377bfd
badgateway666 <badgateway@das-labor.org>
2021-03-17 00:28:49 +0100

    Refactor and implement subclass filter

--- metafun_test.py
+++ metafun_test.py
@@ -5,13 +5,24 @@ import importlib
 import metafun  # Our module to reflect
 
 
-def magic(root_module):
+def magic(root_module, superclasses=None):
     for mod in pkgutil.walk_packages(root_module.__path__, f"{root_module.__name__}."):
         if not mod.ispkg:  # All packages are modules, but not all modules are packages
             for name, cls in inspect.getmembers(importlib.import_module(mod.name), inspect.isclass):
-                yield cls()
+                if not (superclasses is None):
+                    if issubclass(cls, superclasses):  # Filter for subclasses of certain parents
+                        yield cls   # Yield class object
+                else:
+                    yield cls
 
 
 if __name__ == '__main__':
-    for o in magic(metafun):
-        o()
+    # Fetch list of classes in module
+    classes = [cls for cls in magic(metafun)]
+
+    # Instantiate each class once
+    instantiated_objects = [cls() for cls in classes]
+
+    # Call (__call__) each instantiated object
+    for obj in instantiated_objects:
+        obj()  # <=> obj.__call__()
